9135aecd9e5e8edcda766177da9b7a4406a58372
Aurélien <aurelien.strimelle@bstorm.be>
2024-10-15 16:50:42 +0200

    TaskForm - Logic 🦄

--- src/components/TaskForm/TaskForm.tsx
+++ src/components/TaskForm/TaskForm.tsx
@@ -1,32 +1,59 @@
 import { useId } from 'react';
 import { useForm } from 'react-hook-form';
 
+//! Librairie ajouter pour la gestion du formulaire
 // pnpm i react-hook-form @hookform/resolvers yup
-export default function TaskForm() {
 
-  const { register, handleSubmit } = useForm();
+//! Définition des types pour la formulaire
+export type TaskFormData = {
+  name: string;
+  desc?: string;
+  priority: 'low' | 'normal' | 'urgent';
+};
+
+//! Définition du types des props du composant
+type TaskFormProps = {
+  onTaskSubmit?: (data: TaskFormData) => void
+};
+
+//! Le composant
+export default function TaskForm({ 
+  onTaskSubmit = () => {} //No-op
+} : TaskFormProps) {
+
+  const { register, handleSubmit, reset, setFocus } = useForm<TaskFormData>({
+    defaultValues: { name: '', desc: '', priority: 'normal' }
+  });
+
   const inputId = useId();
 
-  const handleTaskSubmit = (data: any) => {
-
-  }
+  const handleTaskSubmit = (data: TaskFormData) => {
+    // Envoi des données vers le composant parent
+    onTaskSubmit(data);
+    
+    // Gestion du focus
+    setFocus('name');
+    
+    // Reset le formulaire
+    reset();
+  };
 
   return (
     <form onSubmit={handleSubmit(handleTaskSubmit)}>
       <div>
-        <label htmlFor={inputId+'name'}>Nom : </label>
-        <input type="text" id={inputId+'name'} />
+        <label htmlFor={inputId + 'name'}>Nom : </label>
+        <input type="text" id={inputId + 'name'}  {...register('name')} />
       </div>
       <div>
         <label htmlFor={inputId + 'desc'}>Desc : </label>
-        <textarea id={inputId + 'desc'} />
+        <textarea id={inputId + 'desc'} {...register('desc')} />
       </div>
       <div>
         <label htmlFor={inputId + 'prio'}>Priorité : </label>
-        <select id={inputId + 'prio'}>
-          <option value="">Basse</option>
-          <option value="">Normal</option>
-          <option value="">Urgent</option>
+        <select id={inputId + 'prio'} {...register('priority')}>
+          <option value='low'>Basse</option>
+          <option value='normal'>Normal</option>
+          <option value='urgent'>Urgent</option>
         </select>
       </div>
       <div>
